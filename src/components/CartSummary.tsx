20bb97b77fcaafae4aab90cf7c627374d894afb6
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-17 01:14:41 +0530

    adding heading in summary

--- src/components/CartSummary.tsx
+++ src/components/CartSummary.tsx
@@ -5,7 +5,10 @@ const CartSummary = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between py-24 px-8 border border-gray-400 rounded-lg">
       <h2>Cart Summary</h2>
-      
+      <h2>Total No. of Items: </h2>
+      <h2>Total Discount: </h2>
+      <h2>Net Amount: </h2>
+
     </div>
   );
 };
